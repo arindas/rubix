fadb6f56941d9f82a50c2429dfdee0a9aa061a18
Arindam Das <arindam123das456@gmail.com>
2018-05-31 21:03:50 +0530

    Modified rubix.structure.Edge: - Moved definition map from Edge.Block to Edge - Removed Edge.Block#rotate()

--- src/main/java/rubix/structure/Edge.java
+++ src/main/java/rubix/structure/Edge.java
@@ -8,30 +8,35 @@ import static rubix.structure.Face.*;
 class Edge {
 
 	static class Block implements Cube.Block {
-		private Map<Direction, Subaxis> definition = Direction.getDefaultDef();
+		private Edge parent;
 		private Map<Subaxis, Cube.Block> adjBlocks = new HashMap<>();
 
 		public Color getColor(Direction d)
-		{ return Color.getColor(definition.get(d)); }
+		{ return Color.getColor(parent.definition.get(d)); }
 
 		public Cube.Block getBlock(Direction d)
-		{ return adjBlocks.get(definition.get(d)); }
+		{ return adjBlocks.get(parent.definition.get(d)); }
 
 		public void putBlock(Direction d, Cube.Block b)
-		{ adjBlocks.put(definition.get(d), b); }
-
-		void rotate(Direction normal, int nTurns) {
-			Direction[] directions = Direction.getPlane(normal);
-			Subaxis[] axes = new Subaxis[directions.length];
-			// normalize nTurns
-			while(nTurns < 0) {nTurns += axes.length;}
-			nTurns %= axes.length;
-
-			for(int i = 0; i < axes.length; i++)
-				axes[(i + nTurns) % axes.length] = definition.get(directions[i]);
-
-			for(int i = 0; i < axes.length; i++)
-				definition.put(directions[i], axes[i]);
+		{ adjBlocks.put(parent.definition.get(d), b); }
+		
+		void joinParent(Edge edge) {
+			Map<Subaxis, Subaxis> oldToNew = new HashMap<>();
+			for(Direction d : Direction.DIRECTIONS) {
+				oldToNew.put(edge.definition.get(d),
+					edge.definition.get(d));
+			}
+			
+			Map<Subaxis, Cube.Block> adjBlocks_ = new HashMap<>();
+			
+			for(Subaxis axis : Subaxis.SUBAXES) {
+				// using the new axis mapped from the old axis
+				// map the value of the old axis to the new axis
+				adjBlocks_.put(oldToNew.get(axis), adjBlocks.get(axis));
+			}
+			
+			adjBlocks = adjBlocks_; 
+			this.parent = edge;
 		}
 	}
 
@@ -40,7 +45,8 @@ class Edge {
 	
 	Direction direction;
 
-	Map<Direction, Direction> normToDir;
+	private Map<Direction, Direction> normToDir;
+	private Map<Direction, Subaxis> definition = Direction.getDefaultDef();
 	
 	Direction getDirection(Direction normal) {
 		Direction d = normToDir.get(normal);
@@ -81,13 +87,13 @@ class Edge {
 
 	void rotate(Direction normal, int nTurns) {
 		setNormal(normal);
-	
+		
 		Direction oldDirection = direction;
 
 		// get plane directions
 		Direction[] dirs = Direction.getPlane(normal);
 		Direction[][] plane = Face.getPlaneDirs(direction);
-		
+		Subaxis[] axes = new Subaxis[dirs.length];
 		// normalize nTurns
 		while(nTurns < 0) nTurns += dirs.length;
 		nTurns %= dirs.length;
@@ -96,14 +102,12 @@ class Edge {
 		int i = 0; for(; i < dirs.length; i++) {
 			if(direction == dirs[i]) break;
 		} direction = dirs[(i + nTurns) % dirs.length];
+		
+		for(i = 0; i < axes.length; i++)
+			axes[(i + nTurns) % axes.length] = definition.get(dirs[i]);
 
-		Edge.Block block = start;
-		do {
-			Edge.Block next = (Edge.Block) block
-				.getBlock(oldDirection);
-			block.rotate(normal, nTurns);
-			block = next;
-		} while(block != end);
+		for(i = 0; i < axes.length; i++)
+			definition.put(dirs[i], axes[i]);
 		
 		normToDir.put(plane[X][NEG], direction);
 		normToDir.put(plane[Y][POS], Direction.getReverse(direction));
